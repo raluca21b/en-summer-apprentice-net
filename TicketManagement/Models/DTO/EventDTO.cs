0d3a4271fa3253fbf05ded492d2a2118f791ba4a
Raluca Bulz <ralubulz@gmail.com>
2023-07-26 11:54:41 +0300

    Modify EventDTO

--- TicketManagement/Models/DTO/EventDTO.cs
+++ TicketManagement/Models/DTO/EventDTO.cs
@@ -3,10 +3,10 @@
     public class EventDTO
     {
         public int EventId { get; set; }
-        public string EventName { get; set; }
-        public string EventDescription { get; set; }
-        public string EventType { get; set; }
-        public string Venue { get; set; }
+        public string? EventName { get; set; }
+        public string? EventDescription { get; set; }
+        public string? EventType { get; set; }
+        public string? Venue { get; set; }
 
     }
 }
